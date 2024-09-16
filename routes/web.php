e728676fffa72af5e2bd4ba34166ac38f81af686
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-09-16 11:03:11 +0700

    update: part 7 (naming route use ZiggyVue)

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,6 @@ use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Home');
-});
+})->name('home');
 
-Route::inertia('/about', 'About', ['user' => 'Jhon Doe']);
+Route::inertia('/about', 'About', ['user' => 'Jhon Doe'])->name('about');
