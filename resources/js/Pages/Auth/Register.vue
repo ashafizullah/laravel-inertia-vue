dc6a9f934ad3febd58300f64c63e9f696354ae29
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-09-16 11:37:51 +0700

    update: part 10 (register controller post)

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,7 +1,7 @@
 <script setup>
-  import { reactive } from 'vue';
+  import { useForm } from '@inertiajs/vue3';
 
-  const form = reactive({
+  const form = useForm({
     name: null,
     email: null,
     password: null,
@@ -9,7 +9,9 @@
   })
 
   const submit = () => {
-    console.log(form);
+    form.post(route('register'),{
+      onError: () => form.reset('password', 'password_confirmation')
+    });
   }
 </script>
 <template>
@@ -22,14 +24,17 @@
       <div class="mb-6">
         <label>Name</label>
         <input type="text" v-model="form.name"/>
+        <small>{{ form.errors.name }}</small>
       </div>
       <div class="mb-6">
         <label>Email</label>
         <input type="text" v-model="form.email"/>
+        <small>{{ form.errors.email }}</small>
       </div>
       <div class="mb-6">
         <label>Password</label>
         <input type="password" v-model="form.password"/>
+        <small>{{ form.errors.password }}</small>
       </div>
       <div class="mb-6">
         <label>Confirm Password</label>
@@ -37,7 +42,7 @@
       </div>
       <div>
         <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
-        <button class="primary-btn">Register</button>
+        <button class="primary-btn" :disabled="form.processing">Register</button>
       </div>
     </form>
   </div>
