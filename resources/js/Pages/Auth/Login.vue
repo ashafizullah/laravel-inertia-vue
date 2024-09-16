7dd753c2ab73276ec894164464c54df85a7907de
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-09-16 12:46:46 +0700

    update: part 12 (login form)

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,31 +3,34 @@
   import TextInput from '../Components/TextInput.vue';
 
   const form = useForm({
-    name: null,
     email: null,
     password: null,
-    password_confirmation: null
+    remember: null
   })
 
   const submit = () => {
-    form.post(route('register'),{
-      onError: () => form.reset('password', 'password_confirmation')
+    form.post(route('login'),{
+      onError: () => form.reset('password')
     });
   }
 </script>
 <template>
-  <Head title="Register"/>
+  <Head title="Login"/>
 
-  <h1 class="title">Register a new account</h1>
+  <h1 class="title">Login to your account</h1>
 
   <div class="w-2/4 mx-auto">
     <form @submit.prevent="submit">
-      <TextInput name="name" v-model="form.name" :message="form.errors.name"/>
       <TextInput name="email" type="email" v-model="form.email" :message="form.errors.email"/>
       <TextInput name="password" type="password" v-model="form.password" :message="form.errors.password"/>
-      <TextInput name="confirm password" type="password" v-model="form.password_confirmation"/>
+      <div class="flex items-center justify-between mb-2">
+        <div class="flex items-center gap-2">
+          <input type="checkbox" v-model="form.remember" id="remember"/>
+          <label for="remember">Remember me</label>
+        </div>
+        <p class="text-slate-600">Need an account? <a :href="route('register')" class="text-link">Register</a></p>
+      </div>
       <div>
-        <p class="text-slate-600 mb-2">Already a user? <a :href="route('login')" class="text-link">Login</a></p>
         <button class="primary-btn" :disabled="form.processing">Register</button>
       </div>
     </form>
