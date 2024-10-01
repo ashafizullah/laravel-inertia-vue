94b2f3bbbf719b7627d8ca5f049146af6ea94e3e
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-10-01 22:05:47 +0700

    update: part 18 (authorization)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) {
@@ -10,7 +11,10 @@ Route::get('/', function (Request $request) {
         $query ->where('name', 'like', '%' . $request->search . '%')
             ->orWhere('email', 'like', '%' . $request->search . '%');
     })->paginate(5)->withQueryString(),
-        'searchTerm' => $request->search
+        'searchTerm' => $request->search,
+        'can' => [
+            'delete_user' => Auth::user() ? Auth::user()->can('delete',User::class) : null
+        ]
     ]);
 })->name('home');
 
